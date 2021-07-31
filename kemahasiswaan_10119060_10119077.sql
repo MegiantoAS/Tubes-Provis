29f304c78d9874d55605239eeda201d9d815ed98
RidhwanAF <72844854+RidhwanAF@users.noreply.github.com>
2021-07-31 23:25:37 +0700

    Update kemahasiswaan_10119060_10119077.sql

--- kemahasiswaan_10119060_10119077.sql
+++ kemahasiswaan_10119060_10119077.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 31, 2021 at 03:03 PM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 7.4.21
+-- Generation Time: Jul 31, 2021 at 06:24 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -85,12 +85,12 @@ CREATE TABLE `t_nilai` (
   `tugas1` double NOT NULL,
   `tugas2` double NOT NULL,
   `tugas3` double NOT NULL,
-  `nilai_uts` double NOT NULL,
-  `nilai_uas` double NOT NULL,
-  `nilai_absen` double NOT NULL,
-  `nilai_tugas` double NOT NULL,
   `uts` double NOT NULL,
   `uas` double NOT NULL,
+  `nilai_absen` double NOT NULL,
+  `nilai_tugas` double NOT NULL,
+  `nilai_uts` double NOT NULL,
+  `nilai_uas` double NOT NULL,
   `nilai_akhir` int(11) NOT NULL,
   `indeks` char(5) NOT NULL,
   `ket` varchar(20) NOT NULL
@@ -100,14 +100,19 @@ CREATE TABLE `t_nilai` (
 -- Dumping data for table `t_nilai`
 --
 
-INSERT INTO `t_nilai` (`kd_nilai`, `nim`, `kd_mk`, `absensi`, `tugas1`, `tugas2`, `tugas3`, `nilai_uts`, `nilai_uas`, `nilai_absen`, `nilai_tugas`, `uts`, `uas`, `nilai_akhir`, `indeks`, `ket`) VALUES
-(3, '10105122', 'IF34348', 0, 0, 0, 0, 0, 0, 40, 0, 0, 0, 0, 'E', 'Tidak Lulus'),
-(4, '10105121', 'IF37325P', 0, 0, 0, 0, 0, 0, 90, 0, 0, 0, 0, 'A', 'Lulus'),
-(5, '10119060', 'IF99192', 0, 0, 0, 0, 0, 0, 90, 0, 0, 0, 0, 'A', 'Lulus'),
-(6, '10105121', 'IF34348', 0, 0, 0, 0, 0, 0, 70, 0, 0, 0, 0, 'B', 'lulus'),
-(7, '10105122', 'IF235359', 0, 0, 0, 0, 0, 0, 40, 0, 0, 0, 0, 'E', 'Tidak Lulus'),
-(8, '10105122', 'IF99192', 0, 0, 0, 0, 0, 0, 100, 0, 0, 0, 0, 'A', 'Lulus'),
-(9, '10119077', 'IF99191', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 'C', 'LULUS');
+INSERT INTO `t_nilai` (`kd_nilai`, `nim`, `kd_mk`, `absensi`, `tugas1`, `tugas2`, `tugas3`, `uts`, `uas`, `nilai_absen`, `nilai_tugas`, `nilai_uts`, `nilai_uas`, `nilai_akhir`, `indeks`, `ket`) VALUES
+(19, '10105120', 'IF34348', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 'A', 'Lulus'),
+(20, '10119060', 'IF99191', 14, 80, 40, 40, 50, 50, 5, 0, 0, 0, 5, 'E', 'Tidak Lulus'),
+(21, '10105122', 'IF256544', 1, 2, 3, 4, 5, 6, 0.3571428571428571, 0, 0, 0, 0, 'E', 'Tidak Lulus'),
+(22, '10105120', 'IF99191', 1, 2, 3, 4, 5, 6, 0.3571428571428571, 0, 0, 0, 0, 'E', 'Tidak Lulus'),
+(23, '10105122', 'IF256544', 14, 50, 25, 30, 50, 50, 5, 0, 0, 0, 5, 'E', 'Tidak Lulus'),
+(24, '10105122', 'IF99191', 10, 25, 25, 25, 20, 20, 3.5714285714285716, 0, 0, 0, 4, 'E', 'Tidak Lulus'),
+(25, '10119060', 'IF256544', 14, 25, 55, 25, 10, 52, 5, 0, 0, 0, 5, 'E', 'Tidak Lulus'),
+(26, '10105120', 'IF34348', 1000, 55, 55, 55, 55, 55, 357.14285714285717, 0, 0, 0, 357, 'E', 'Tidak Lulus'),
+(27, '10119060', 'IF37325P', 500, 55, 55, 55, 55, 55, 178.57142857142858, 0, 0, 0, 179, 'E', 'Tidak Lulus'),
+(28, '10105120', 'IF256544', 300, 22, 22, 22, 22, 22, 107.14285714285714, 0, 0, 0, 107, 'E', 'Tidak Lulus'),
+(29, '10105120', 'IF99191', 200, 33, 33, 33, 33, 33, 71.42857142857143, 0, 0, 0, 71, 'B', 'Lulus'),
+(30, '10105120', 'IF256544', 40, 20, 20, 20, 20, 20, 14.285714285714286, 0, 0, 0, 14, 'E', 'Tidak Lulus');
 
 --
 -- Indexes for dumped tables
@@ -141,7 +146,7 @@ ALTER TABLE `t_nilai`
 -- AUTO_INCREMENT for table `t_nilai`
 --
 ALTER TABLE `t_nilai`
-  MODIFY `kd_nilai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `kd_nilai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- Constraints for dumped tables
